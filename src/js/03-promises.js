ec6dc64cfc039cefa28c247cb66c1667f283462b
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2023-11-13 23:10:28 +0200

    fixed last

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,45 +1,4 @@
 import Notiflix from 'notiflix';
-/*
-* @param1 {string}: Required, a text in string format.
-* @param2 {function | Object}: Optional, a callback function that will be called when the notification element has been clicked. Or, extending the initialize options with the new options for each notification element.
-* @param3 {Object}: Optional, extending the initialize options with new the options for each notification element. (If the second parameter has been already used for a callback function.)
-*/
-
-// // e.g. Only message
-// Notiflix.Notify.success('Sol lucet omnibus');
-
-// Notiflix.Notify.failure('Qui timide rogat docet negare');
-
-// Notiflix.Notify.warning('Memento te hominem esse');
-
-// Notiflix.Notify.info('Cogito ergo sum');
-
-// e.g. Message with a callback
-// Notiflix.Notify.success(
-//   'Click Me',
-//   function cb() {
-//     // callback
-//   },
-// );
-
-// e.g. Message with the new options
-// Notiflix.Notify.success(
-//   'Click Me',
-//   {
-//     timeout: 6000,
-//   },
-// );
-
-// e.g. Message with callback, and the new options
-// Notiflix.Notify.success(
-//   'Click Me',
-//   function cb() {
-//     // callback
-//   },
-//   {
-//     timeout: 4000,
-//   },
-// );
 
 const form = document.querySelector('form');
 form.addEventListener('submit', onFormInputData);
@@ -68,15 +27,16 @@ function onFormInputData(event) {
     const promise = createPromise(i, delayInput)
     promise.then(({ position, delay }) => {
         Notiflix.Notify.success(
-          `✅ Fulfilled promise ${position} in ${delay}ms`
+          `✅ Fulfilled promise ${position + 1} in ${delay}ms`,
+          { timeout: 10000, }
         );
       })
       .catch(({ position, delay }) => {
         Notiflix.Notify.failure(
-          `❌ Rejected promise ${position} in ${delay}ms`
+          `❌ Rejected promise ${position + 1} in ${delay}ms`,
+          { timeout: 10000, }
         );
       });
-    // На кожній єтерації додаємо - щоб наступний виклик проміса відбувся за новою затримакою "Delay step"
     delayInput += stepInput;
   }
 }
